ec7af6a7c8424a2b2a81ee7fb741df8b67c884bf
Muhammad Rana <muhammadrana523@gmail.com>
2022-01-06 01:36:35 +0500

    Changes

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', 'AuthController@register');
 Route::post('login', 'AuthController@login');
 Route::post('logout', 'AuthController@logout')->middleware('auth:sanctum');
+
 //BookingController Route
 Route::post('createBooking', 'BookingController@createBooking')->middleware('auth:sanctum');
 Route::post('scheduleBooking', 'BookingController@scheduleBooking')->middleware('auth:sanctum');
