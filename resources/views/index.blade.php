5605120635bf1f159270b9d40fd5db0053e1c8ac
Muhammad Rana <muhammadrana523@gmail.com>
2022-02-11 19:08:33 +0500

    Changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -38,7 +38,7 @@
                             <div class="row d-flex justify-content-center">
                                 <div class="col">
                                     <p class="text-dark mb-0 font-weight-semibold">Users</p>
-                                    <h3 class="m-0">{{ count($users) }}</h3>
+{{--                                    <h3 class="m-0">{{ count($users) }}</h3>--}}
                                 </div>
                             </div>
                         </div><!--end card-body-->
