c4fd0cd6e10640d076d2c842516b447f025a09a0
Muhammad Rana <muhammadrana523@gmail.com>
2022-02-11 19:09:13 +0500

    Changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -50,7 +50,7 @@
                             <div class="row d-flex justify-content-center">
                                 <div class="col">
                                     <p class="text-dark mb-0 font-weight-semibold">Orders</p>
-                                    <h3 class="m-0">{{ count($orders) }}</h3>
+{{--                                    <h3 class="m-0">{{ count($orders) }}</h3>--}}
                                 </div>
                             </div>
                         </div><!--end card-body-->
